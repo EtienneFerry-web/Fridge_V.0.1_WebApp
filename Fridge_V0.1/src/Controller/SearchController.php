e9f028cba9ecf05070f2e6d6a059bbf808f52021
Etienne Ferry <etienne.ferry@ccicampus.fr>
2026-04-13 17:25:09 +0200

    Feat: Enhance search functionality with additional query parameters

--- Fridge_V0.1/src/Controller/SearchController.php
+++ Fridge_V0.1/src/Controller/SearchController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\RecetteRepository;
+use App\Repository\RegimeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,16 +11,36 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class SearchController extends AbstractController
 {
-    #[Route('/search', name: 'app_search')]
-    public function index(Request $request): Response
-    {
+    #[Route('/search', name: 'app_search', methods: ['GET'])]
+    public function index(
+        Request $request,
+        RecetteRepository $recetteRepository,
+        RegimeRepository $regimeRepository
+    ): Response {
         $query = $request->query->get('q', '');
+        $arrDifficulte          = $request->query->all('difficulte');
+        $arrRegimes             = $request->query->all('regimes');
+        $strOrigine             = $request->query->get('origine', '');
+        $intTempsPreparationMax = $request->query->getInt('temps_preparation_max', 120);
+        $strSortBy              = $request->query->get('sort_by', 'pertinence');
 
-        $recipes = [];
+        $recette                = $recetteRepository->findBySearch(
+            $query,
+            $arrDifficulte,
+            $arrRegimes,
+            $strOrigine,
+            $intTempsPreparationMax,
+            $strSortBy
+        );
 
         return $this->render('search/index.html.twig', [
-            'recipes' => $recipes,
+            'recipes' => $recette,
             'query'   => $query,
+            'difficulte' => $arrDifficulte,
+            'regimes' => $arrRegimes,
+            'origine' => $strOrigine,
+            'temps_preparation_max' => $intTempsPreparationMax,
+            'sort_by' => $strSortBy,
         ]);
     }
 }
