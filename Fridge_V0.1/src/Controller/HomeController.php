91c761843e049c4421d24652d71b6c45b5f95def
Etienne Ferry <etienne.ferry@ccicampus.fr>
2026-04-13 07:46:42 +0200

    Feat:Maj Affichage profil & home index.html.twig

--- Fridge_V0.1/src/Controller/HomeController.php
+++ Fridge_V0.1/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(RecetteRepository $objRecetteRepository): Response
     {
+        $arrRecettesCarousel = $objRecetteRepository->findBy([], ['id' => 'DESC'], 6);
+        $arrRecettes         = $objRecetteRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'arrRecettesCarousel' => $arrRecettesCarousel,
+            'arrRecettes'         => $arrRecettes,
         ]);
     }
 }
