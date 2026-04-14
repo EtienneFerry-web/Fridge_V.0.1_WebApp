f0b41df6cc17738140c97a2f8742d1f6056faec4
Etienne Ferry <etienne.ferry@ccicampus.fr>
2026-04-14 09:34:29 +0200

    Feat: Implementation pagination via KnpPaginatorBundle

--- Fridge_V0.1/src/Controller/HomeController.php
+++ Fridge_V0.1/src/Controller/HomeController.php
@@ -4,16 +4,30 @@ namespace App\Controller;
 
 use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(RecetteRepository $objRecetteRepository): Response
-    {
+    public function index(
+        RecetteRepository  $objRecetteRepository,
+        PaginatorInterface $paginator,
+        Request            $request
+    ): Response {
         $arrRecettesCarousel = $objRecetteRepository->findBy([], ['id' => 'DESC'], 6);
-        $arrRecettes         = $objRecetteRepository->findAll();
+
+        $queryRecettes = $objRecetteRepository->createQueryBuilder('r')
+            ->orderBy('r.recetteCreatedAt', 'DESC')
+            ->getQuery();
+
+        $arrRecettes = $paginator->paginate(
+            $queryRecettes,
+            $request->query->getInt('page', 1),
+            8
+        );
 
         return $this->render('home/index.html.twig', [
             'arrRecettesCarousel' => $arrRecettesCarousel,
