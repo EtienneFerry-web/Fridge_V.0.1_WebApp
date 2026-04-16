0121dae542a9118dae0fce548508902931b6178f
Etienne Ferry <etienne.ferry@ccicampus.fr>
2026-04-16 16:12:35 +0200

    Installation dépendence Zenstruck

--- Fridge_V0.1/config/bundles.php
+++ Fridge_V0.1/config/bundles.php
@@ -19,4 +19,5 @@ return [
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
